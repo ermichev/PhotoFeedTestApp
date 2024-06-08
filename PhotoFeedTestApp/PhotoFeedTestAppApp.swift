f79126b3808e71c309334e9ec79e72308232a49b
Alexander Ermichev <keri-kk@yandex.ru>
2024-06-08 15:42:34 +0200

    Initial Commit

---
+++ PhotoFeedTestApp/PhotoFeedTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFeedTestAppApp.swift
+//  PhotoFeedTestApp
+//
+//  Created by Alexander Ermichev on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFeedTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
